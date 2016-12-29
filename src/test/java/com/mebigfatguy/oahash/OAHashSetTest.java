1d32dec7836a5f802310629cea91c8f109ee0a1d
Dave Brosius <dbrosius@mebigfatguy.com>
2016-12-29 11:04:20 -0500

    initial set test

---
+++ src/test/java/com/mebigfatguy/oahash/OAHashSetTest.java
@@ -0,0 +1,71 @@
+/*
+ * oahash - An open addressing hash implementation for Maps and Sets
+ * Copyright 2016 MeBigFatGuy.com
+ * Copyright 2016 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.oahash;
+
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OAHashSetTest {
+    private Set<String> s;
+
+    @Before
+    public void setUp() {
+        s = new OAHashSet<>();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testPutNull() {
+
+        s.add(null);
+    }
+
+    @Test
+    public void testAddAndOverwrite() {
+        Assert.assertTrue(s.add("test"));
+        Assert.assertFalse(s.add("test"));
+    }
+
+    @Test
+    public void testFillWithoutExpansion() {
+        for (int i = 0; i < 11; i++) {
+            String o = String.valueOf(i);
+            s.add(o);
+        }
+
+        for (int i = 0; i < 11; i++) {
+            String o = String.valueOf(i);
+            Assert.assertTrue(s.contains(o));
+        }
+    }
+
+    @Test
+    public void testFillWithExpansion() {
+        for (int i = 0; i < 100; i++) {
+            String o = String.valueOf(i);
+            s.add(o);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            String o = String.valueOf(i);
+            Assert.assertTrue(s.contains(o));
+        }
+    }
+}
