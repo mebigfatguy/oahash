ff98e672150a23f244e3e2a0868408415ea313e5
Dave Brosius <dbrosius@mebigfatguy.com>
2016-12-29 11:04:08 -0500

    initial set test

--- src/test/java/com/mebigfatguy/oahash/OAHashMapTest.java
+++ src/test/java/com/mebigfatguy/oahash/OAHashMapTest.java
@@ -1,3 +1,20 @@
+/*
+ * oahash - An open addressing hash implementation for Maps and Sets
+ * Copyright 2016 MeBigFatGuy.com
+ * Copyright 2016 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.mebigfatguy.oahash;
 
 import java.util.Map;
@@ -41,7 +58,7 @@ public class OAHashMapTest {
             Assert.assertEquals(s, m.get(s));
         }
     }
-    
+
     @Test
     public void testFillWithExpansion() {
         for (int i = 0; i < 100; i++) {
