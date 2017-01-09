6c94da5a0827f27a7986b3e3850878d0d87d5ffd
Dave Brosius <dbrosius@mebigfatguy.com>
2017-01-09 18:53:04 -0500

    update cp dates

--- src/test/java/com/mebigfatguy/oahash/OAHashSetTest.java
+++ src/test/java/com/mebigfatguy/oahash/OAHashSetTest.java
@@ -1,7 +1,7 @@
 /*
  * oahash - An open addressing hash implementation for Maps and Sets
- * Copyright 2016 MeBigFatGuy.com
- * Copyright 2016 Dave Brosius
+ * Copyright 2016-2017 MeBigFatGuy.com
+ * Copyright 2016-2017 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
